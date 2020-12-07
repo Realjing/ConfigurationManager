d03e0d829f6f9b6191fcbfdafeb75572e4c6294c
Realjing <2231782857@qq.com>
2020-12-07 13:54:47 +0800

    添加例子

--- AspNetCore.ConfigurationManager/AspNetCore.ConfigurationManager.TestConsole/Program.cs
+++ AspNetCore.ConfigurationManager/AspNetCore.ConfigurationManager.TestConsole/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using AspNetCore;
 
 namespace AspNetCoreConfigurationManager.TestConsole
@@ -8,9 +9,15 @@ namespace AspNetCoreConfigurationManager.TestConsole
         static void Main(string[] args)
         {
 
-            var v = ConfigurationManager.GetConnectionString("Test");
-            Console.WriteLine($"读取到：{v}");
+            //var v = ConfigurationManager.GetConnectionString("Test");
+            //Console.WriteLine($"读取到：{v}");
+            var t = ConfigurationManager.GetValue<Test>(Path.Combine($"{Directory.GetCurrentDirectory()}/dllconfig/appsettings.json"), "Test");
+            Console.WriteLine($"读取到：{t.Value}");
             Console.ReadKey(true);
         }
+    }
+    public class Test
+    {
+        public string Value { get; set; }
     }
 }
